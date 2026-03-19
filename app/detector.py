c23ec147ca12795922682c66035f08c58629461b
Bhakthi-Shetty7811 <bhakthi.shetty7811@gmail.com>
2026-03-20 04:24:13 +0530

    changes in face analysis model

--- app/detector.py
+++ app/detector.py
@@ -3,7 +3,7 @@ from insightface.app import FaceAnalysis
 class Detector:
 
     def __init__(self):
-        self.app = FaceAnalysis(name="buffalo_l")
+        self.app = FaceAnalysis(name="buffalo_s")
         self.app.prepare(ctx_id=0)
 
     def detect(self, image):
